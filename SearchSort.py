83ad4f9d9e60a7affe19bf6a538313b3670f219a
Luke Fleming <lmfleming1001@gmail.com>
2022-12-09 13:28:55 +0000

    Ok make sure to fix the array stuff with binSearch

--- SearchSort.py
+++ SearchSort.py
@@ -13,69 +13,76 @@ def setupData():
     print(dataSet)
     return dataSet
 
-def main():
-    def linSearch(search):
-        start = time.time()
-        found = [False]
-        for i in range(len(data)):
-            if int(search) == data[i]:
-                found[0] = True
-                found.append(i)
-        print(found)
-        if found[0] == True:   
-            for j in range(len(found)-1):
-                print(f"Found {search} at postition {found[j+1]}")
-                
-        else:
-                print(f"{search} not found")   
-        end = time.time()
-        print(end - start)    
-    def bubSort():
-        start = time.time()
-        swapped = True
-        while swapped == True:
-            swapped = False
-            for i in range(len(data)-1):
-                if data[i] > data[i + 1]:
-                    swapped = True
-                    temp = data[i + 1]
-                    data[i + 1] = data[i]
-                    data[i] = temp
-        end = time.time()
-        print(end - start)
-    def insSort():
-        start = time.time()
-        for i in range(1, len(data)):
-            temp = data[i]
-            pos = i - 1
-            while pos >= 0 and data[pos] > temp:
-                data[pos + 1] = data[pos]
-                pos = pos-1
-            data[pos + 1] = temp
-        end = time.time()
-        print(end - start)
-    def binSearch(search):
-        start = time.time()
-        first = 0
-        last = len(data)-1
-        mid = (first + last) // 2
-        while data[mid] != search and first < last:
-            print(f"{first} {mid} {last}")
-            mid = (first + last) // 2
-            if search > mid:
-                first = mid + 1
-            else:
-                last = mid
-            
-        if data[mid] == search:
-            print("Found")
-            
-        else:
-            print("Not found")
-                
-        end = time.time()
-        input(end - start)
 
+def linSearch(data, search):
+    start = time.time()
+    found = [False]
+    for i in range(len(data)):
+        if int(search) == data[i]:
+            found[0] = True
+            found.append(i)
+    print(found)
+    if found[0] == True:   
+        for j in range(len(found)-1):
+            print(f"Found {search} at postition {found[j+1]}")
+            
+    else:
+            print(f"{search} not found")   
+    end = time.time()
+    print(end - start)    
+
+def bubSort(data):
+    start = time.time()
+    swapped = True
+    while swapped == True:
+        swapped = False
+        for i in range(len(data)-1):
+            if data[i] > data[i + 1]:
+                swapped = True
+                temp = data[i + 1]
+                data[i + 1] = data[i]
+                data[i] = temp
+    end = time.time()
+    print(end - start)
+
+def insSort(data):
+    start = time.time()
+    for i in range(1, len(data)):
+        temp = data[i]
+        pos = i - 1
+        while pos >= 0 and data[pos] > temp:
+            data[pos + 1] = data[pos]
+            pos = pos-1
+        data[pos + 1] = temp
+    end = time.time()
+    print(end - start)
+
+def binSearch(data, search):
+    start = time.time()
+    first = 0
+    last = len(data)-1
+    mid = (first + last) // 2
+    while data[mid] != search and first < last:
+        print(f"{first} {mid} {last}")
+        
+        if search > mid:
+            first = mid
+        elif search < mid:
+            last = mid
+        mid = (first + last) // 2
+        
+    if data[mid] == search:
+        print("Found")
+        
+    else:
+        print("Not found")
+            
+    end = time.time()
+    input(end - start)
+
+
+def main():
+    
 
     data = setupData()
     while True:
@@ -90,19 +97,19 @@ def main():
 
         if choice == 1:
             print("Linear search")
-            linSearch(input("Enter number to find: "))
+            linSearch(data, input("Enter number to find: "))
             
         elif choice == 2:
             print("Bubble sort")
-            bubSort()          
+            bubSort(data)          
                 
         elif choice == 3:
             print("Insertion sort")
-            insSort()
+            insSort(data)
         
         elif choice == 4:
             print("Binary search")
-            binSearch(int(input("Enter number to find: ")))
+            binSearch(data, int(input("Enter number to find: ")))
 
         elif choice == 5:
             print("Quicksort")
